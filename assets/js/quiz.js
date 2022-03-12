aba0b6a7e699636a7bea1a2ebe256c330d72f9d9
TaimurHasan <taimurhasan11@gmail.com>
2022-03-12 12:41:17 -0500

    added question object to begin creating and testing question function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,5 +1,17 @@
 var quizBtn = document.querySelector(".quiz-btn");
+var timer = document.querySelector("#timer");
+var time = 0;
 
+//creating array to hold objects for each quiz question
+var questions = [
+    {
+        "question": "Commonly used data types do NOT include:",
+        "choices": ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+        "correct-choice": "3. Alerts"
+    }
+];
+
+console.log(questions[0].question);
 
 // clean up function to remove existing html from main section
 var cleanUp = function() {
@@ -8,7 +20,23 @@ var cleanUp = function() {
     mainBody.innerHTML = "";
 }
 
+//function to start timer 
+var timerStart = function () {
+    time = 75;
+
+    // time interval with function that will decrement time variable by 1 every second
+    var timeInterval = window.setInterval(
+        function () {
+            if(time > 0) {
+                timer.textContent = "Time: " + time;
+                time--;
+            } else {
+                clearInterval(timeInterval);
+            }
+        }, 1000
+    )
+}
 
 //add click event listener to the quiz button to start functions
-quizBtn.addEventListener("click", cleanUp);
+quizBtn.addEventListener("click", timerStart);
 
