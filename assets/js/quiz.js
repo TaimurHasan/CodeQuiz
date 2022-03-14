6f27cafb453028e3b1b3a827a54bb5e84ec9a526
TaimurHasan <taimurhasan11@gmail.com>
2022-03-13 20:49:40 -0400

    refactoring code

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -19,6 +19,22 @@ var questions = [
         "choices": ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],
         "correctChoice": "Parenthesis"
     },
+    {
+        "question": "Arrays in JavaScript can be used to store:",
+        "choices": ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+        "correctChoice": "All of the above"
+    },
+    {
+        "question": "String values must be enclosed within ______ when being assigned to variables",
+        "choices": ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        "correctChoice": "Quotes"
+    },
+    {
+        "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
+        "choices": ["JavaScript", "Terminal Bash", "for loops", "console.log"],
+        "correctChoice": "console.log"
+    }
+
 ];
 
 // clean up function to remove existing html from main section
